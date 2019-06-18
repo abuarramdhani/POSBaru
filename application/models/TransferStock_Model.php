258d6ecd9ecdc20c4ac24e6122d1dcb7a10e4c9f
Rizki <rizkihardinas@gmail.com>
2019-06-18 19:56:34 +0700

    Update TransferStock_Model.php

--- application/models/TransferStock_Model.php
+++ application/models/TransferStock_Model.php
@@ -9,7 +9,7 @@ class TransferStock_Model extends CI_Model
 	{
 		parent::__construct();
 	}
-	public function record_customers_count()
+	public function record_transferstock_count()
     {
         $this->db->order_by('id', 'DESC');
         $query = $this->db->get('transfer_stock');
@@ -18,11 +18,12 @@ class TransferStock_Model extends CI_Model
         return $query->num_rows();
     }
 
-    public function fetch_customers_data($limit, $start)
+    public function fetch_transferstock_data($limit, $start)
     {
-        $this->db->order_by('id', 'DESC');
-        $this->db->limit($limit, $start);
-        $query = $this->db->get('transfer_stock');
+        $query = $this->db->query("SELECT o.name as outlet_asal,ot.name as outlet_tujuan,transfer_stock.* FROM outlets o JOIN transfer_stock ON o.id = transfer_stock.first_outlet JOIN outlets ot ON transfer_stock.second_outlet = ot.id ORDER BY transfer_stock.id DESC LIMIT $start,$limit");
+        // $this->db->order_by('transfer_stock.id', 'DESC');
+        // $this->db->limit($limit, $start);
+        // $query = $this->db->get('transfer_stock');
 
         $result = $query->result();
 
@@ -32,12 +33,16 @@ class TransferStock_Model extends CI_Model
     }
     function check_stock($data){
     	$this->db->where($data);
-    	$query = $this->db->get('inventory');
+    	$query = $this->db->get('inventory',$data);
     	return $query->result_array();	
     }
-    function update_stock(){
-        $this->db->where($data);
-        $this->db->update('inventory');
+    function get_barang(){
+        $query = $this->db->query('SELECT DISTINCT(products.name), inventory.product_code FROM products JOIN inventory ON products.code = inventory.product_code');
+        return $query->result();  
+    }
+    function update_stock($id,$data){
+        $this->db->where($id);
+        $this->db->update('inventory',$data);
     }
 }
  ?>
