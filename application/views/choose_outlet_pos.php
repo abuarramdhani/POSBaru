20cd14bafd56b47151aa59863e49e0b503926111
rizkihardinas <36495798+rizkihardinas@users.noreply.github.com>
2019-06-24 11:46:30 +0700

    Update choose_outlet_pos.php

--- application/views/choose_outlet_pos.php
+++ application/views/choose_outlet_pos.php
@@ -1,20 +1,11 @@
 <?php
-    require_once 'includes/pos_header.php';
+    require_once 'includes/pos_header2.php';
 ?>
 <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
 
 <section id="content">
 	<section class="vbox">
-		<header class="header bg-white b-b">
-			<p>Welcome to <?php echo $lang_dashboard; ?></p>
-			<a href="<?=base_url()?>index.php/pos" class="btn btn-success pull-right btn-sm" id="new-note">
-				<i class="fa fa-adjust"></i> <?php echo $lang_pos; ?>
-			</a>
-		</header>
-
 		<section class="scrollable wrapper">
-		
-
 	<div class="row">
 		<div class="col-lg-12">
 			<h1 class="page-header" style="text-align: center; padding-bottom: 0px; margin-bottom: 10px;">
